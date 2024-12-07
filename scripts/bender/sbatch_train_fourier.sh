1fc1b645ddd5d8d2ca56687b3d21a27525a40c97
Niclas Pillath <npillath.github@stimlabs.dev>
2024-12-07 14:24:38 +0100

    Add sbatch script

---
+++ scripts/bender/sbatch_train_fourier.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --partition=A40short
+#SBATCH --time=02:00:00
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --threads-per-core=1
+#SBATCH -e out/%j.err
+#SBATCH -o out/%j.out
+
+module load Miniforge3/24.1.2-0 CUDA/12.4.0
+source ~/.bashrc
+conda activate Forensics
+
+export PYTHONPATH=$PYTHONPATH:$(pwd):$(pwd)/src
+
+python -O -m freqdect.train_classifier \
+  --dataset-root-dir /home/npillath/datasets/image \
+  --dataset-positive afhqv2_11k_512 ffhq_11k_512 \
+  --dataset-negative afhqv2_stylegan3_11k_512 ffhq_stylegan3_11k_512 \
+  --model cnn --features fourier
+
+echo "SBATCH completed"
